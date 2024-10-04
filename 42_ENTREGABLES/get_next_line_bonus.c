2ad1cd75743d75a6b621c47423975130ad896f36
Juanma Gomez <juanma.gomez.martin@gmail.com>
2024-10-04 13:53:59 +0200

    v1.4 presentacion + bonus

--- 42_ENTREGABLES/get_next_line_bonus.c
+++ 42_ENTREGABLES/get_next_line_bonus.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juagomez <juagomez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/27 21:59:55 by juagomez          #+#    #+#             */
-/*   Updated: 2024/10/04 13:49:18 by juagomez         ###   ########.fr       */
+/*   Created: 2024/10/04 11:43:00 by juagomez          #+#    #+#             */
+/*   Updated: 2024/10/04 13:47:09 by juagomez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 static char	*get_final_part_line(char *total_line)
 {
@@ -100,71 +100,62 @@ static char	*read_file(int fd, char *total_line)
 
 char	*get_next_line(int fd)
 {
-	static char	*buffer;
+	static char	*buffer[1024];
 	char		*next_line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 	{
-		free (buffer);
-		buffer = NULL;
+		free (buffer[fd]);
+		buffer[fd] = NULL;
 		return (NULL);
 	}
-	buffer = read_file(fd, buffer);
-	if (!buffer)
+	buffer[fd] = read_file(fd, buffer[fd]);
+	if (!buffer[fd])
 		return (NULL);
-	next_line = get_cleaned_line(buffer);
-	buffer = get_final_part_line(buffer);
+	next_line = get_cleaned_line(buffer[fd]);
+	buffer[fd] = get_final_part_line(buffer[fd]);
 	return (next_line);
 }
 
-/* int	main(void)
+/* int main(void) 
 {
-	int		fd = open("text_00.txt", O_RDONLY);
+    int     fd_0;
+    int     fd_1;
+    int     fd_2;
+    char    *next_line;
+    int     count_line;
+
+    count_line = 0;
+    fd_0 = open("text_00.txt", O_RDONLY);
+    fd_1 = open("text_01.txt", O_RDONLY);
+    fd_2 = open("text_02.txt", O_RDONLY);
 	
-	int index_read;
-	char	*buffer;
-	char	*new_line;
-	char	*final_line;
-
-	// 1º COPIAR LINEA COMPLETA HASTA L BUFFER QUE 
-	// INCLUYE SALTO LINEA -> READ_FILE()
-	index_read = read(fd, malloc(10 + 1), 10);
-	buffer = read_file(fd, buffer);
-	//printf("tamaño buffer -> %d \n", index_read);
-	//printf("Linea bruta -> %s<-final\n", buffer);
-
-	// 2º CONSEGUIR LINEA NETA HASTA SALTO LINEA -> GIVE_LINE()
-	new_line = give_line(buffer);
-	//printf("Linea neta -> %s \n", new_line);
-
-	// 3º CONSEGUIR FINAL LINEA TEXTO DESPUES DE SALTO LINEA
-	final_line = get_final_line(buffer);
-	printf("final linea -> %s \n", final_line);
-
-	free(buffer);
-	free(new_line);
-	free(final_line);
-
-	// 4º TEST FUNCION GET_NEXT_LINE COMPLETA
-	char	*next_line;
-	int     count_line;
-
-	count_line = 0;
-	if (fd == -1) 
+    if (fd_0 == -1 || fd_1 == -1 || fd_2 == -1) 
     {
         printf("Error al abrir archivo");
         return (1);
     }
-	while ((next_line = get_next_line(fd)))
-	{
-		count_line++;
-		printf("[%d]-> %s\n",count_line, next_line);
-		free(next_line);
-	}
-	if (close(fd) == -1)
+
+    while ((next_line = get_next_line(fd_0))) {
+        count_line++;
+        printf("[%d]-> %s\n", count_line, next_line);
+        free(next_line);
+    }
+    while ((next_line = get_next_line(fd_1))) {
+        count_line++;
+        printf("[%d]-> %s\n", count_line, next_line);
+        free(next_line);
+    }
+    while ((next_line = get_next_line(fd_2))) {
+        count_line++;
+        printf("[%d]-> %s\n", count_line, next_line);
+        free(next_line);
+    }
+
+    if ((close(fd_0) == -1) || (close(fd_1) == -1) || (close(fd_2) == -1))
 	{
         printf("Error al cerrar archivo");
         return (1);
     }
-	return (0);
+    return (0);
 } */
---
+++ 42_ENTREGABLES/get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juagomez <juagomez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 11:43:20 by juagomez          #+#    #+#             */
+/*   Updated: 2024/10/04 12:08:51 by juagomez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+int		ft_strlcpy(char *dest, const char *src, unsigned int destsize);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strjoin(char const *str1, char const *str2);
+int		ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int character);
+
+#endif
